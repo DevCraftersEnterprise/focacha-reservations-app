18e411269743d443afaaf26cdc0b11758e4fdc9e
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-03-28 10:11:19 -0600

    Add AppStrings class for managing app string constants

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,6 @@
+abstract final class AppStrings {
+  static const appName = 'Reservaciones';
+  static const loginTitle = 'Iniciar sesión';
+  static const dashboardTitle = 'Dashboard';
+  static const reservationsTitle = 'Reservaciones';
+}
