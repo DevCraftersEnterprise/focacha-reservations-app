c678577a00ef33ae4ec031aa8d0dd3a72b77bc63
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-03-28 10:21:26 -0600

    Add ReservationApp class for managing the main application structure

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../core/constants/app_strings.dart';
+import '../core/theme/app_theme.dart';
+
+class ReservationApp extends StatelessWidget {
+  const ReservationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: AppStrings.appName,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+    );
+  }
+}
