3f7df6b954087488a09030427b725251aa85668d
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-03-28 09:51:03 -0600

    Add AppColors class for consistent color management

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const primary = Color(0xFF8B1E1E);
+  static const primaryDark = Color(0xFF5E1313);
+  static const accent = Color(0xFFD9A441);
+
+  static const background = Color(0xFFFFFDFC);
+  static const surface = Color(0xFFF7F3EE);
+  static const border = Color(0xFFE7E0D8);
+
+  static const textPrimary = Color(0xFF1F1A17);
+  static const textSecondary = Color(0xFF6B625B);
+
+  static const successBg = Color(0xFFE8F5EC);
+  static const successText = Color(0xFF1E7A3E);
+
+  static const errorBg = Color(0xFFFDECEC);
+  static const errorText = Color(0xFFB42318);
+}
